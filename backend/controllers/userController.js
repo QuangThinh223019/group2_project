876ea3c1f9c7d504dc1f162b8c23342d8b1e38a1
LeNhatDuy <duylenhatt@gmail.com>
2025-10-04 20:06:56 +0700

    Copy thu muc backend tu backend sang database

---
+++ backend/controllers/userController.js
@@ -0,0 +1,25 @@
+const User = require('../models/User');
+
+// GET /users
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// POST /users
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email
+    });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
