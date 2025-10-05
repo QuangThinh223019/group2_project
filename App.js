512e7d5776ba542cb1b3ef92aa8ed93d72bf7a68
QuangThinh223019 <thinh223019@student.nctu.edu.vn>
2025-10-05 15:01:17 +0700

    Thêm file App.js cho backend

---
+++ App.js
@@ -0,0 +1,10 @@
+console.log('App backend running');
+// App.js (backend)
+console.log("This is the backend App.js file");
+
+// Giả lập một hàm chạy backend
+function startBackend() {
+  console.log("Backend is running...");
+}
+
+startBackend();
