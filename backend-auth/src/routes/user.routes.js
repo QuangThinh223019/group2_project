ff0c9596457b36241ef8d435024bbdfc9ca12996
PhamHuynhNhu-hihi <huynhnhuphamj@gmail.com>
2025-10-11 21:08:29 +0700

    Lấy folder backend-auth từ backend-admin

---
+++ backend-auth/src/routes/user.routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const auth = require('../middlewares/auth');
+const rbac = require('../middlewares/rbac');
+const ctrl = require('../controllers/user.controller');
+
+router.post('/',   auth(), rbac('admin'), ctrl.addUser); //thêm 
+router.get('/',    auth(), rbac('admin'), ctrl.listUsers);//lấy ds
+router.put('/:id', auth(), rbac('admin'), ctrl.updateUser);//cập nhật
+router.delete('/:id', auth(), rbac('admin'), ctrl.deleteUser);//xóa 
+
+module.exports = router;
