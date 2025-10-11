4b4f66b10af252401cb178d46554e9a15e6a1771
PhamHuynhNhu-hihi <huynhnhuphamj@gmail.com>
2025-10-11 16:32:40 +0700

    Lấy folder backend-auth từ nhánh backend-auth cho hd 2

--- backend-auth/src/models/User.js
+++ backend-auth/src/models/User.js
@@ -4,7 +4,10 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true, select: false },
-  role: { type: String, enum: ['user', 'admin'], default: 'user' }
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  avatarUrl: { type: String, default: '' },
+  resetToken: { type: String, select: false, default: null },
+  resetTokenExp: { type: Date, default: null }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
