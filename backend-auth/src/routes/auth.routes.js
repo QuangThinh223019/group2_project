9a9b82101104b938283da3e7a3baad75d18f54c6
ThinhCaoQuang <thinh.caoq@gmail.com>
2025-10-16 16:09:25 +0700

    cập nhậtl lại auth.routes

--- backend-auth/src/routes/auth.routes.js
+++ backend-auth/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const ctrl = require('../controllers/auth.controller');
 
+
 router.post('/signup', ctrl.signup);
 router.post('/login', ctrl.login);
 router.post('/logout', ctrl.logout);
